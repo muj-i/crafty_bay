f6f3d75afef9357f80781dde50d684725bacafd6
MUJ <99077742+muj-i@users.noreply.github.com>
2023-09-06 17:53:28 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/muj/i/crafty_bay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muj.i.crafty_bay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/application/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CraftyBayApp extends StatelessWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
